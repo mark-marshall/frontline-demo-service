01f730f2b408c17d67c08aa31afc0ac21b553e47
Mark Marshall <mmarshall@twilio.com>
2021-07-23 14:36:39 +0100

    Replace consts with Twilio service urls

--- src/providers/customers.js
+++ src/providers/customers.js
@@ -1,87 +1,88 @@
-// Map between customer address and worker identity
-// Used to determine to which worker route a new conversation with a particular customer
-//
-// {
-//     customerAddress: workerIdentity
-// }
-//
-// Example:
-//     {
-//         'whatsapp:+12345678': 'john@example.com'
-//     }
-const customersToWorkersMap = {};
+const axios = require('axios');
 
-// Customers list
-// Example:
-// [
-//   {
-//      customer_id: 98,
-//      display_name: 'Bobby Shaftoe',
-//      channels: [
-//          { type: 'email', value: 'bobby@example.com' },
-//          { type: 'sms', value: '+123456789' },
-//          { type: 'whatsapp', value: 'whatsapp:+123456789' }
-//      ],
-//      links: [
-//          { type: 'Facebook', value: 'https://facebook.com', display_name: 'Social Media Profile' }
-//      ],
-//      details:{
-//          title: "Information",
-//          content: "Status: Active" + "\n\n" + "Score: 100"
-//      },
-//      worker: 'john@example.com'
-//   }
-// ]
+const customersToWorkersMap = { '+447557955350': 'mmarshall@twilio.com' };
 
-const customers = [];
-
-const findWorkerForCustomer = async (customerNumber) => customersToWorkersMap[customerNumber];
+const findWorkerForCustomer = async (customerNumber) => {
+  const customersToWorkersMapRes = await axios.get(
+    'https://ukise-frontline-1182.twil.io/customers-workers-map.json'
+  );
+  const customersToWorkersMap = customersToWorkersMapRes.data;
+  return customersToWorkersMap[customerNumber];
+};
 
 const findRandomWorker = async () => {
-    const onlyUnique = (value, index, self) => {
-        return self.indexOf(value) === index;
-    }
+  const customersToWorkersMapRes = await axios.get(
+    'https://ukise-frontline-1182.twil.io/customers-workers-map.json'
+  );
+  const customersToWorkersMap = customersToWorkersMapRes.data;
+  const onlyUnique = (value, index, self) => {
+    return self.indexOf(value) === index;
+  };
+  s;
+  const workers = Object.values(customersToWorkersMap).filter(onlyUnique);
+  const randomIndex = Math.floor(Math.random() * workers.length);
 
-    const workers = Object.values(customersToWorkersMap).filter(onlyUnique)
-    const randomIndex = Math.floor(Math.random() * workers.length)
-
-    return workers[randomIndex]
-}
+  return workers[randomIndex];
+};
 
 const getCustomersList = async (worker, pageSize, anchor) => {
-    const workerCustomers = customers.filter(customer => customer.worker === worker);
-    const list = workerCustomers.map(customer => ({
-        display_name: customer.display_name,
-        customer_id: customer.customer_id,
-        avatar: customer.avatar,
-    }));
+  const customersRes = await axios.get(
+    'https://ukise-frontline-1182.twil.io/customers.json'
+  );
+  const customers = customersRes.data;
 
-    if (!pageSize) {
-        return list
-    }
+  const workerCustomers = customers.filter(
+    (customer) => customer.worker === worker
+  );
+  const list = workerCustomers.map((customer) => ({
+    display_name: customer.display_name,
+    customer_id: customer.customer_id,
+    avatar: customer.avatar,
+  }));
 
-    if (anchor) {
-        const lastIndex = list.findIndex((c) => String(c.customer_id) === String(anchor))
-        const nextIndex = lastIndex + 1
-        return list.slice(nextIndex, nextIndex + pageSize)
-    } else {
-        return list.slice(0, pageSize)
-    }
+  if (!pageSize) {
+    return list;
+  }
+
+  if (anchor) {
+    const lastIndex = list.findIndex(
+      (c) => String(c.customer_id) === String(anchor)
+    );
+    const nextIndex = lastIndex + 1;
+    return list.slice(nextIndex, nextIndex + pageSize);
+  } else {
+    return list.slice(0, pageSize);
+  }
 };
 
 const getCustomerByNumber = async (customerNumber) => {
-    return customers.find(customer => customer.channels.find(channel => String(channel.value) === String(customerNumber)));
+  const customersRes = await axios.get(
+    'https://ukise-frontline-1182.twil.io/customers.json'
+  );
+  const customers = customersRes.data;
+
+  return customers.find((customer) =>
+    customer.channels.find(
+      (channel) => String(channel.value) === String(customerNumber)
+    )
+  );
 };
 
 const getCustomerById = async (customerId) => {
-    return customers.find(customer => String(customer.customer_id) === String(customerId));
+  const customersRes = await axios.get(
+    'https://ukise-frontline-1182.twil.io/customers.json'
+  );
+  const customers = customersRes.data;
+
+  return customers.find(
+    (customer) => String(customer.customer_id) === String(customerId)
+  );
 };
 
 module.exports = {
-    customersToWorkersMap,
-    findWorkerForCustomer,
-    findRandomWorker,
-    getCustomerById,
-    getCustomersList,
-    getCustomerByNumber
+  findWorkerForCustomer,
+  findRandomWorker,
+  getCustomerById,
+  getCustomersList,
+  getCustomerByNumber,
 };
